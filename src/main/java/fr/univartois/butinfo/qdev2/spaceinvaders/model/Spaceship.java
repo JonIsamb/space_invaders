ce74f5ffa3c027d2af5a96749fd8e9ed277b27fe
Bastien <bastien@nizart.me>
2022-09-27 11:21:34 +0200

    Définition de la classe du vaisseau

---
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/Spaceship.java
@@ -0,0 +1,8 @@
+package fr.univartois.butinfo.qdev2.spaceinvaders.model;
+
+import fr.univartois.butinfo.qdev2.spaceinvaders.model.movables.AbstractMovable;
+import fr.univartois.butinfo.qdev2.spaceinvaders.view.Sprite;
+
+public class Spaceship extends AbstractMovable {
+
+}
