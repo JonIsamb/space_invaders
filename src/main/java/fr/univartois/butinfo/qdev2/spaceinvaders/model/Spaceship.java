847c96f8fb959293610cfacef1ec5e9d90d0e80b
Bastien <bastien@nizart.me>
2022-09-27 11:22:33 +0200

    Re-définition du constructeur avec super constructeur

--- src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/Spaceship.java
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/Spaceship.java
@@ -4,5 +4,16 @@ import fr.univartois.butinfo.qdev2.spaceinvaders.model.movables.AbstractMovable;
 import fr.univartois.butinfo.qdev2.spaceinvaders.view.Sprite;
 
 public class Spaceship extends AbstractMovable {
+    /**
+     * Crée une nouvelle instance de AbstractMovable.
+     *
+     * @param game      Le jeu dans lequel l'objet évolue.
+     * @param xPosition La position en x initiale de l'objet.
+     * @param yPosition La position en y initiale de l'objet.
+     * @param sprite    L'instance de {@link Sprite} représentant l'objet.
+     */
+    protected Spaceship(SpaceInvadersGame game, double xPosition, double yPosition, Sprite sprite) {
+        super(game, xPosition, yPosition, sprite);
+    }
 
 }
