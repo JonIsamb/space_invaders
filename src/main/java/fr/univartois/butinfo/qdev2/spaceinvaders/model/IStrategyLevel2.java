2c57faa23bc2fa82a9cc302a352b572f9ab2ce50
Gregoire Legrand <gregoire.legrand56@gmail.com>
2022-10-18 17:31:01 +0200

    add methode shot et strategy :construction:

---
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/IStrategyLevel2.java
@@ -0,0 +1,2 @@
+package fr.univartois.butinfo.qdev2.spaceinvaders.model;public interface IStrategyLevel2 {
+}
---
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/ShotLevel1.java
@@ -0,0 +1,12 @@
+package fr.univartois.butinfo.qdev2.spaceinvaders.model;
+
+public class AlienCounterAttack implements IStrategy
+{
+
+    @Override
+    public void counterAttack(IMovable alien) {
+
+    }
+
+
+}
