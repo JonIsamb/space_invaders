21bacaf2d4d3e39306ff1cf76afe7f62afb898a9
Jonathan Isambourg <jonathan.isambourg1@gmail.com>
2022-10-19 13:05:13 +0200

    Ajout de la première stratégie de mouvement des Aliens

---
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/movables/moves/AlienMoves1Strategy.java
@@ -0,0 +1,11 @@
+package fr.univartois.butinfo.qdev2.spaceinvaders.model.movables.moves;
+
+import fr.univartois.butinfo.qdev2.spaceinvaders.model.movables.Alien;
+
+public class AlienMoves1Strategy implements IAlienMovesStrategy {
+    @Override
+    public void update(Alien alien) {
+        alien.setVerticalSpeed(alien.getVerticalSpeed() * 1.02);
+        alien.setHorizontalSpeed(-alien.getHorizontalSpeed());
+    }
+}
