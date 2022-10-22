c857cede59cc55aac0f4bd87a94deae3f7fe2504
Jonathan Isambourg <jonathan.isambourg1@gmail.com>
2022-10-22 13:40:49 +0200

    Fix de la 1ère stratégie de déplacement des Aliens

--- src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/movables/moves/AlienMoves1Strategy.java
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/movables/moves/AlienMoves1Strategy.java
@@ -5,7 +5,9 @@ import fr.univartois.butinfo.qdev2.spaceinvaders.model.movables.Alien;
 public class AlienMoves1Strategy implements IAlienMovesStrategy {
     @Override
     public void update(Alien alien, boolean contactWithBorder) {
-        alien.setVerticalSpeed(alien.getVerticalSpeed()*1.02);
-        alien.setHorizontalSpeed(-alien.getHorizontalSpeed());
+        if (contactWithBorder){
+            alien.setVerticalSpeed(alien.getVerticalSpeed()*1.02);
+            alien.setHorizontalSpeed(-alien.getHorizontalSpeed());
+        }
     }
 }
